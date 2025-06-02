19b43e73ac10520ea348e3e463554f522677a2be
brandingmax <bmaxagencia@gmail.com>
2025-06-01 21:01:34 -0300

    main sem aspas

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { CssBaseline } from '@mui/material'
+import CssBaseline from '@mui/material/CssBaseline'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
